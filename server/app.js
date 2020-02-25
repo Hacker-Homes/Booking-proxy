650b3ed49377d5bd4457f5514393c7f6c5eba783
Jake Wija <jakewija@icloud.com[core]>
2020-02-24 16:16:03 -0800

    change bundle src location

--- server/app.js
+++ server/app.js
@@ -4,27 +4,44 @@ const proxy = require('http-proxy-middleware');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const morgan = require('morgan');
 
 const PORT = 3030
-
-const { routes } = require('./config.json');
-
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('dev'))
 
-for (route of routes) {
-  app.use(route.route,
-    proxy({
-      target: route.address,
-      pathRewrite: (path, req) => {
-        return path.split('/').slice(2).join('/');
-      }
-    })
-  );
-}
+app.get('/', (req, res, next) => {
+  
+})
+
+// for (route of routes) {
+//   app.use(route.route,
+//     proxy({
+//       target: route.address,
+//       pathRewrite: (path, req) => {
+//         return path.split('/').slice(2).join('/');
+//       }
+//     })
+//   );
+// }
+
+// {
+//   "routes": [
+//   {
+//     "route": "/booking",
+//     "address": "http://44.233.161.53:5555/booking"
+//   },
+//   {
+//     "route": "/room",
+//     "address": "http://44.233.161.53:5555/room"
+//   }
+// ]
+// }
 
 app.listen(PORT, () => {
   console.log(`Proxy listening on port ${PORT}`);
